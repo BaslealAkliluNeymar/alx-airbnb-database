657957cb63bff75ffb57521416d07641e3de5799
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2025-05-18 20:31:22 +0300

    Not empty

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE User(
 
 
 CREATE TABLE Property (
-    property_id   UUID DEFAULT uuid_generate_v4() Primary Key,
+    property_id UUID DEFAULT uuid_generate_v4() Primary Key,
     FOREIGN KEY(user_id) host_id  REFERENCES User(user_id),
     name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
@@ -24,8 +24,8 @@ CREATE TABLE Property (
 
 CREATE TABLE Booking (
     booking_id UUID DEFAULT uuid_generate_v4() Primary Key,
-    property_id FOREIGN KEY(property_id) REFERENCES Property(property_id),
-    user_id FOREIGN KEY(user_id) REFERENCES User(user_id),
+    FOREIGN KEY(property_id)  property_id REFERENCES Property(property_id),
+    FOREIGN KEY(user_id) user_id  REFERENCES User(user_id),
     start_date TIMESTAMP NOT NULL,
     end_date TIMESTAMP NOT NULL,
     total_price DECIMAL NOT NULL,
@@ -36,8 +36,8 @@ CREATE TABLE Booking (
 
 CREATE TABLE Review (
     review_id UUID DEFAULT uuid_generate_v4() Primary Key,
-    property_id FOREIGN KEY(property_id) REFERENCES Property(property_id),
-    user_id FOREIGN KEY(user_id) REFERENCES User(user_id),
+    FOREIGN KEY(property_id) property_id  REFERENCES Property(property_id),
+    FOREIGN KEY(user_id) user_id  REFERENCES User(user_id),
     rating INT CHECK (rating >= 1 AND rating <= 5),
     comment TEXT,
     created_at TIMESTAMP,
@@ -46,7 +46,7 @@ CREATE TABLE Review (
 )
 CREATE TABLE Payment (
     payment_id UUID DEFAULT uuid_generate_v4() Primary Key,
-    booking_id FOREIGN KEY(booking_id) REFERENCES Booking(booking_id),
+    FOREIGN KEY(booking_id) booking_id REFERENCES Booking(booking_id),
     amount DECIMAL NOT NULL,
     payment_date TIMESTAMP NOT NULL,
     status ENUM('pending', 'completed', 'failed') NOT NULL
@@ -56,9 +56,9 @@ CREATE TABLE Payment (
 
 CREATE TABLE Message (
     message_id UUID DEFAULT uuid_generate_v4() Primary Key,
-    sender_id FOREIGN KEY(user_id) REFERENCES User(user_id),
-    receiver_id FOREIGN KEY(user_id) REFERENCES User(user_id),
-    property_id FOREIGN KEY(property_id) REFERENCES Property(property_id),
+    FOREIGN KEY(user_id) sender_id  REFERENCES User(user_id),
+    FOREIGN KEY(user_id) receiver_id  REFERENCES User(user_id),
+    FOREIGN KEY(property_id) property_id  REFERENCES Property(property_id),
     content TEXT NOT NULL,
     sent_at TIMESTAMP
     sender_id UUID NOT NULL
---
+++ database-script-0x02/README.md
@@ -0,0 +1,1 @@
+This is an insert script inorder to seed the database for airbnb database
