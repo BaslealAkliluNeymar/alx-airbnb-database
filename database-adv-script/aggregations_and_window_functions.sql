59bb24c749d08716fe9c582898070f3f012a6c52
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2025-05-25 20:53:02 +0300

    Both

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,7 +10,8 @@ GROUP BY
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number
 FROM 
     bookings
 GROUP BY 
