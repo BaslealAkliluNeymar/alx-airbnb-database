9ce72a9f95643d7ee5680ac4367555829088c097
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2025-05-25 20:56:30 +0300

    optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,6 +1,24 @@
+
+EXPLAIN ANALYZE
+SELECT 
+    user_id, 
+    COUNT(*) AS total_bookings
+FROM
+    bookings
+GROUP BY
+    user_id;
+
 CREATE INDEX idx_user_id ON bookings(user_id);
 CREATE INDEX idx_property_id ON bookings(property_id);
 CREATE INDEX idx_created_at ON bookings(created_at);
 
 
+EXPLAIN ANALYZE
+SELECT 
+    user_id, 
+    COUNT(*) AS total_bookings
+FROM
+    bookings
+GROUP BY
+    user_id
 
